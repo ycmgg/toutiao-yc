ebf19cf0275e57ec967af14116a8d9d32e07125f
YCMGG <2358517223@qq.com>
2022-08-23 15:27:40 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    
     <router-view/>
+
+    <div>
+      <i class="toutiao toutiao-wenda"></i>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
+    
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+// 使用Vant 组件库
+Vue.use(Vant);
 Vue.config.productionTip = false
 
 new Vue({
